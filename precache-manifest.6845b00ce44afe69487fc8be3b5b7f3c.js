dad65485841d38c82ddd53be268f91e0aa61c115
INDERJEET <inderbittu00@gmail.com>
2020-06-17 17:59:25 +0530

    Updates

---
+++ precache-manifest.6845b00ce44afe69487fc8be3b5b7f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8a695617936cc7b72388098f136868",
+    "url": "/my_trip/index.html"
+  },
+  {
+    "revision": "7fe3125ab68384611484",
+    "url": "/my_trip/static/css/main.6c31c46a.chunk.css"
+  },
+  {
+    "revision": "b8853b8bba2e5724ba3a",
+    "url": "/my_trip/static/js/2.ded5c290.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/my_trip/static/js/2.ded5c290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe3125ab68384611484",
+    "url": "/my_trip/static/js/main.f3267912.chunk.js"
+  },
+  {
+    "revision": "e5c9bedc6112967bebb0",
+    "url": "/my_trip/static/js/runtime-main.4969d17d.js"
+  },
+  {
+    "revision": "684f0401f1cd5c4b9e39202e2ba923f6",
+    "url": "/my_trip/static/media/back.684f0401.jpeg"
+  }
+]);
